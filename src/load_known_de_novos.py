b483266d399245e62ffb510f02d0eb51d2812eae
Jeremy McRae <jm33@sanger.ac.uk>
2014-04-07 17:10:21 +0100

    removed xlrd requirement

--- src/load_known_de_novos.py
+++ src/load_known_de_novos.py
@@ -4,8 +4,6 @@
 from __future__ import print_function
 from __future__ import division
 
-from xlrd import open_workbook
-
 # functional consequences, minus any indel consequences
 functional_consequences = ["splice_donor_variant", "splice_acceptor_variant",\
     "initiator_codon_variant", "missense_variant", "transcript_amplification", \
@@ -21,50 +19,25 @@ def load_known_de_novos(filename):
     """ load known mutations into dict indexed by HGNC ID.
     """
     
-    # sometimes the input is an excel file
-    if filename.endswith("xlsx"):
-        # open an xlsx file containing the current list of de novo mutations in
-        # probands studied to date
-        book = open_workbook(filename, on_demand=True)
-        sheet = book.sheet_by_name("DNMs_nonred")
-        header = table.row_values(0)
-        
-        # convert the excel file to a list of lists
-        table = []
-        row = 0
-        while row < table.nrows - 1:
-            row += 1
-            values = table.row_values(row)
-            table.append(values)
-    # otherwise assume the file is a tab-separated text file
-    else:
-        f = open(filename, "r")
-        read = f.readlines()
-        header = read[0].strip().split("\t")
-        
-        # convert the tab separated file to a list of lists
-        table = []
-        for line in read[1:]:
-            table.append(line.rstrip().split("\t"))
+    f = open(filename, "r")
+    header = f.readline().strip().split("\t")
     
     # get the positions of the columns that we are interested in
     gene_name_col = header.index("gene_name")
     position_col = header.index("pos")
     consequence_col = header.index("consequence")
     snp_or_indel_col = header.index("snp_or_indel")
-    unused_consequences = set([])
     
     genes_dict = {}
-    for values in table:
-        # print(values)
-        gene = values[gene_name_col]
-        position = values[position_col]
-        consequence = values[consequence_col]
-        snp_or_indel = values[snp_or_indel_col]
+    for line in f:
+        line = line.rstrip().split("\t")
+        gene = line[gene_name_col]
+        position = line[position_col]
+        consequence = line[consequence_col]
+        snp_or_indel = line[snp_or_indel_col]
         
         # don't include de novos that aren't functionally important
         if consequence not in functional_consequences:
-            unused_consequences.add(consequence)
             continue
         
         # ignore indels (some splice_acceptor_variants (in the
