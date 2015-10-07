0e5f7427d512444cfa25425c4c383c41346ee881
Jeremy McRae <jm33@sanger.ac.uk>
2015-10-07 09:58:56 +0100

    refactored loading de novos

--- denovonear/load_de_novos.py
+++ denovonear/load_de_novos.py
@@ -1,8 +1,7 @@
 """ loads a file containing known de novo mutations
 """
 
-from __future__ import print_function
-from __future__ import division
+from __future__ import print_function, division
 
 missense = ["missense_variant", "stop_lost", "inframe_deletion",
     "inframe_insertion", "coding_sequence_variant", "protein_altering_variant"]
@@ -13,43 +12,47 @@ lof = ["stop_gained", "splice_acceptor_variant",
     
 synonymous = ["synonymous_variant"]
 
-def load_de_novos(filename, exclude_indels=True, exclude_snvs=False):
+def load_de_novos(path):
     """ load mutations into dict indexed by HGNC ID.
+    
+    Args:
+        path: path to file containing de novo data. This should have five tab-
+            separated columns e.g.
+            hgnc  chr  pos      consequence         var_type
+            CTC1  17   8139190  missense_variant    snv
+            CTC1  17   8139191  frameshift_variant  indel
+    
+    Returns:
+        dictionary of missense and lof counts for each gene, indexed by HGNC symbols
     """
     
-    f = open(filename, "r")
-    header = f.readline().strip().split("\t")
-    
     genes = {}
-    for line in f:
-        # ignore header lines
-        if line.startswith("gene") or line.lower().startswith("HGNC"):
-            continue
-        
-        line = line.rstrip().split("\t")
-        gene = line[0]
-        position = int(line[2]) - 1
-        consequence = line[3]
-        var_type = line[4]
-        
-        # ignore indels (some splice_acceptor_variants (in the
-        # functional_consequences) are indels
-        if exclude_indels and "INDEL" in var_type.upper():
-            continue
-        if exclude_snvs and "SNV" in var_type.upper():
-            continue
-        
-        # trim out variants that are missing data
-        if gene == "" or gene == "." or position == "NA":
-            continue
-        
-        if gene not in genes:
-            genes[gene] = {"missense": [], "nonsense": []}
-        
-        if consequence in missense:
-            genes[gene]["missense"].append(position)
-        elif consequence in lof:
-            genes[gene]["nonsense"].append(position)
+    with open(path, "r") as handle:
+        header = handle.readline().strip().split("\t")
+        for line in handle:
+            
+            line = line.rstrip().split("\t")
+            gene = line[0]
+            position = int(line[2]) - 1
+            consequence = line[3]
+            var_type = line[4]
+            
+            # ignore indels (some splice_acceptor_variants (in the
+            # functional_consequences) are indels
+            if "indel" in var_type.lower():
+                continue
+            
+            # trim out variants that are missing data
+            if gene == "" or gene == "." or position == "NA":
+                continue
+            
+            if gene not in genes:
+                genes[gene] = {"missense": [], "nonsense": []}
+            
+            if consequence in missense:
+                genes[gene]["missense"].append(position)
+            elif consequence in lof:
+                genes[gene]["nonsense"].append(position)
         
     return genes
         
