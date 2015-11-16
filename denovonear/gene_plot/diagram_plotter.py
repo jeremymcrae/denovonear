8ed14a38e40339320592227465b0331d62f20924
Jeremy McRae <jm33@sanger.ac.uk>
2015-11-16 16:49:07 +0000

    exclude more shades of white

--- denovonear/gene_plot/diagram_plotter.py
+++ denovonear/gene_plot/diagram_plotter.py
@@ -20,7 +20,7 @@ class DiagramPlotter(GenomicPlot, TranscriptPlot, DomainPlot):
     # colors close to white, as they are difficult to distingush from background
     global white
     white = ["aliceblue", "azure", "floralwhite", "ghostwhite", "honeydew",
-        "ivory", "oldlace", "seashell", "snow", "white"]
+        "ivory", "linen", "mintcream", "oldlace", "seashell", "snow", "white"]
     colorset = webcolors.css3_names_to_hex.keys()
     colorset = [ x for x in colorset if x not in white ]
     
