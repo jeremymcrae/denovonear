9ef48433d73710059d91d945fefc7233e403d6ec
Jeremy McRae <jm33@sanger.ac.uk>
2016-02-25 15:45:42 +0000

    Revert "allow for installs without an initial cython"
    This reverts commit e89e9a2708ca657a4fdb8f9d62c3dac59d892dd2.

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 
 import sys
-from setuptools import setup, Extension
+from setuptools import setup
+from distutils.core import Extension
+from Cython.Build import cythonize
 
 EXTRA_COMPILE_ARGS = ["-std=c++0x"]
 
 if sys.platform == "darwin":
     EXTRA_COMPILE_ARGS = ["-stdlib=libc++"]
 
-class lazy_cythonize(list):
-    def __init__(self, callback):
-        self._list, self.callback = None, callback
-    def c_list(self):
-        if self._list is None: self._list = self.callback()
-        return self._list
-    def __iter__(self):
-        for e in self.c_list(): yield e
-    def __getitem__(self, ii): return self.c_list()[ii]
-    def __len__(self): return len(self.c_list())
-
-def extensions():
-    from Cython.Build import cythonize
-    ext = Extension("denovonear.weights",
+module1 = cythonize([
+    Extension("denovonear.weights",
         extra_compile_args=EXTRA_COMPILE_ARGS,
         sources=["denovonear/weights.pyx",
             "denovonear/weighted_choice.cpp",
             "denovonear/simulate.cpp"],
-        language="c++")
-    return cythonize([ext])
+        language="c++"),
+    ])
 
 setup (name="denovonear",
         description='Package to examine de novo clustering',
@@ -45,5 +35,5 @@ setup (name="denovonear",
             "Development Status :: 3 - Alpha",
             "License :: OSI Approved :: MIT License",
         ],
-        ext_modules=lazy_cythonize(extensions),
+        ext_modules=module1,
         test_suite="tests")
