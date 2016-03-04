86c3afd3dc1995691d4c3745b9f64734b5c53dce
Jeremy McRae <jm33@sanger.ac.uk>
2016-03-04 14:35:42 +0000

    reorganise unit tests for p-value simulation

--- tests/test_simulate_p_value.py
+++ tests/test_simulate_p_value.py
@@ -14,10 +14,13 @@ from denovonear.simulate import get_p_value
 class TestGetPValuePy(unittest.TestCase):
     """ unit test the simulation of p-values
     """
+    
+    def setUp(self):
         
-    def test_get_p_value(self):
-        """ check that get_p_value() works correctly
-        """
+        self.transcript = self.construct_gene()
+        self.rates = self.get_rates(self.transcript)
+    
+    def construct_gene(self):
         
         chrom = "1"
         name = "TEST"
@@ -33,26 +36,75 @@ class TestGetPValuePy(unittest.TestCase):
         transcript.add_cds_sequence(cds)
         transcript.add_genomic_sequence(gdna)
         
+        return transcript
+    
+    def get_rates(self, tx):
+        
         # load the sequence contect mutation rates, then assess each site in the
         # CDS.
         path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data',
             'forSanger_1KG_mutation_rate_table.txt')
         mut_dict = load_mutation_rates(path)
-        rates = SiteRates(transcript, mut_dict)
         
+        return SiteRates(tx, mut_dict)
+    
+    def test_get_p_value_single_de_novo(self):
+        """ check that get_p_value() works correctly
+        """
+        
+        # check that we don't assess transcripts with a single de novo
+        iterations = 10000
+        cq = 'missense'
+        de_novos = [5]
+        
+        (obs, p_value) = get_p_value(self.transcript, self.rates, iterations, cq, de_novos)
+        self.assertEqual((obs, p_value), ('NA', 'NA'))
+    
+    def test_get_p_value(self):
+        """
+        """
+        
+        iterations = 10000
         cq = 'missense'
         de_novos = [5, 5]
-        iterations = 10000
-        
-        (obs, p_value) = get_p_value(transcript, rates, iterations, cq, de_novos)
+        (obs, p_value) = get_p_value(self.transcript, self.rates, iterations, cq, de_novos)
         
         self.assertTrue(p_value < 0.04)
         self.assertEqual(obs, '0.0')
         
+    def test_get_p_value_nonsignificant(self):
+        """ check for de novos spread across the gene
+        """
+        
+        iterations = 10000
+        cq = 'missense'
         de_novos = [5, 58]
         
-        (obs, p_value) = get_p_value(transcript, rates, iterations, cq, de_novos)
+        (obs, p_value) = get_p_value(self.transcript, self.rates, iterations, cq, de_novos)
         
         self.assertTrue(p_value == 1.0)
         self.assertEqual(obs, '53.0')
         
+    def test_get_p_value_lofs(self):
+        """ check for loss of function rates
+        """
+        
+        # construct two different tx objects, for the same gene, since otherwise
+        # the p-values from different runs are the same
+        tx1 = self.construct_gene()
+        rates1 = self.get_rates(tx1)
+        
+        iterations = 10000
+        cq = 'loss_of_function'
+        de_novos = [5, 6]
+        (obs_1, p_1) = get_p_value(tx1, rates1, iterations, cq, de_novos)
+        
+        tx2 = self.construct_gene()
+        rates2 = self.get_rates(tx2)
+        
+        # make sure we can use the string 'lof' to get loss-of-function rates
+        iterations = 10000
+        cq = 'lof'
+        (obs_2, p_2) = get_p_value(tx2, rates2, iterations, cq, de_novos)
+        self.assertEqual(obs_1, obs_2)
+        self.assertTrue(abs(p_1 - p_2) < 0.01)
