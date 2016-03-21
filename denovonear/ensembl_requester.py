58c3665991c32cfeb080e38bed87642648c7d037
Jeremy McRae <jm33@sanger.ac.uk>
2016-03-21 15:08:27 +0000

    allow all ensembl version 4 requests

--- denovonear/ensembl_requester.py
+++ denovonear/ensembl_requester.py
@@ -71,7 +71,7 @@ class EnsemblRequest(object):
         major = release[0]
         minor = release[1]
         
-        if major != "4" or minor not in ["0", "1", "2", "3", "4"]:
+        if major != "4":
             raise ValueError("check ensembl api version")
         
     def open_url(self, url, headers):
