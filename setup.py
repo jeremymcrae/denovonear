17a9d72fa60c73dfda321ae6142c2b0b3f7a60c6
Jeremy McRae <jm33@sanger.ac.uk>
2017-02-21 13:24:02 +0000

    update version number

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ weights = cythonize([
 
 setup (name="denovonear",
         description='Package to examine de novo clustering',
-        version="0.2.1",
+        version="0.3.0",
         author="Jeremy McRae",
         author_email="jeremy.mcrae@sanger.ac.uk",
         license="MIT",
