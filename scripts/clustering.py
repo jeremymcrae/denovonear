8239879420026866a51091fb7f14c5384511e05a
Jeremy McRae <jmcrae@illumina.com>
2018-01-12 13:25:05 -0800

    tidy up clustering and transcript identification scripts

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -16,68 +16,51 @@ def get_options():
     """ get the command line switches
     """
     
-    parser = argparse.ArgumentParser(description="Examines the proximity" \
+    parser = argparse.ArgumentParser(description="Examines the proximity "
         "clustering of de novo mutations in genes.")
-    parser.add_argument("--in", dest="input", required=True, help="Path to" \
-        "file listing known mutations in genes. See example file in data folder" \
+    parser.add_argument("--in", dest="input", required=True, help="Path to "
+        "file listing known mutations in genes. See example file in data folder "
         "for format.")
-    parser.add_argument("--out", dest="output", required=True, help="output \
-        filename")
+    parser.add_argument("--out", required=True, help="output filename")
     parser.add_argument("--rates",
         help="Path to file containing sequence context-based mutation rates.")
-    parser.add_argument("--deprecated-genes", dest="deprecated_genes_path",
-        help="deprecated gene IDs filename")
-    parser.add_argument("--genome-build", dest="genome_build", choices=["grch37",
-        "GRCh37", "grch38", "GRCh38"], default="grch37", help="Genome build " \
+    parser.add_argument("--genome-build", choices=["grch37",
+        "GRCh37", "grch38", "GRCh38"], default="grch37", help="Genome build "
         "that the de novo coordinates are based on (GrCh37 or GRCh38")
-    parser.add_argument("--cache-folder", dest="cache_dir",
-        default=os.path.join(os.path.dirname(__file__), "cache"), help="folder" \
+    parser.add_argument("--cache-folder",
+        default=os.path.join(os.path.dirname(__file__), "cache"), help="folder "
         "to cache Ensembl data into (defaults to clustering code directory)")
     
-    args = parser.parse_args()
-    
-    args.genome_build = args.genome_build.lower()
-    
-    return args
+    return parser.parse_args()
 
 def main():
     
     args = get_options()
     
     # load all the data
-    ensembl = EnsemblRequest(args.cache_dir, args.genome_build)
+    ensembl = EnsemblRequest(args.cache_folder, args.genome_build.lower())
     mut_dict = load_mutation_rates(args.rates)
-    
-    old_gene_ids = {}
-    # only load the old gene ID converter if we have specified the file
-    if args.deprecated_genes_path is not None:
-        old_gene_ids = get_deprecated_gene_ids(args.deprecated_genes_path)
-    
-    known_de_novos = load_de_novos(args.input)
+    de_novos = load_de_novos(args.input)
     
     output = open(args.output, "w")
     output.write("gene_id\tmutation_category\tevents_n\tdist\tprobability\n")
     
     iterations = 1000000
-    for symbol in sorted(known_de_novos):
+    for symbol in sorted(de_novos):
         
-        de_novos = known_de_novos[symbol]
+        de_novos = de_novos[symbol]
         
         if len(de_novos["missense"] + de_novos["nonsense"]) < 2:
             continue
-        
-        # fix HGNC IDs that have been discontinued in favour of other gene IDs
-        if symbol in old_gene_ids:
-            symbol = old_gene_ids[gene_id]
         
         probs = cluster_de_novos(symbol, de_novos, iterations, ensembl, mut_dict)
         
         if probs is None:
             continue
         
-        output.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(symbol, "missense", \
+        output.write("{}\t{}\t{}\t{}\t{}\n".format(symbol, "missense",
             len(de_novos["missense"]), probs["miss_dist"], probs["miss_prob"]))
-        output.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(symbol, "nonsense", \
+        output.write("{}\t{}\t{}\t{}\t{}\n".format(symbol, "nonsense",
             len(de_novos["nonsense"]), probs["nons_dist"], probs["nons_prob"]))
 
 if __name__ == '__main__':
--- scripts/identify_transcripts.py
+++ scripts/identify_transcripts.py
@@ -5,13 +5,10 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-import sys
 import os
-import math
 import argparse
 
-from denovonear.load_gene import construct_gene_object, get_deprecated_gene_ids, \
-    count_de_novos_per_transcript, minimise_transcripts
+from denovonear.load_gene import count_de_novos_per_transcript, minimise_transcripts
 from denovonear.ensembl_requester import EnsemblRequest
 from denovonear.site_specific_rates import SiteRates
 from denovonear.load_de_novos import load_de_novos
@@ -20,68 +17,49 @@ def get_options():
     """ get the command line switches
     """
     
-    parser = argparse.ArgumentParser(description="Identify transcripts \
-        for a gene containing de novo events.")
-    parser.add_argument("--de-novos", dest="input", required=True, help="Path to \
-        file listing de novo variants in genes.")
-    parser.add_argument("--out", dest="output", required=True, help="output \
-        filename")
-    parser.add_argument("--deprecated-genes", dest="deprecated_genes", \
-        help="deprecated gene IDs filename")
-    parser.add_argument("--genome-build", dest="genome_build", choices=["grch37",
-        "GRCh37", "grch38", "GRCh38"], default="grch37", help="Genome build "+ \
-        "that the de novo coordinates are based on (GrCh37 or GRCh38")
-    parser.add_argument("--cache-folder", dest="cache_folder", \
-        default=os.path.join(os.path.dirname(__file__), "cache"), help="folder \
-        to cache Ensembl data into (defaults to clustering code directory)")
+    parser = argparse.ArgumentParser(description="Identify transcripts "
+        "for a gene containing de novo events.")
+    parser.add_argument("--de-novos", required=True, help="Path to "
+        "file listing de novo variants in genes.")
+    parser.add_argument("--out", required=True, help="output filename")
+    parser.add_argument("--genome-build", choices=["grch37", "GRCh37", "grch38",
+        "GRCh38"], default="grch37", help="Genome build that the de novo "
+            "coordinates are based on (GrCh37 or GRCh38")
+    parser.add_argument("--cache-folder",
+        default=os.path.join(os.path.dirname(__file__), "cache"), help="folder "
+            "to cache Ensembl data into (defaults to clustering code directory)")
     
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("--all-transcripts", action="store_true", default=False,\
-        help="Flag if you want to identify all transcripts with more than" + \
+    group.add_argument("--all-transcripts", action="store_true", default=False,
+        help="Flag if you want to identify all transcripts with more than "
         "one de novo on it.")
-    group.add_argument("--minimise-transcripts", action="store_true", \
-        default=False, help="Flag if you want to identify the minimal set of" +\
+    group.add_argument("--minimise-transcripts", action="store_true",
+        default=False, help="Flag if you want to identify the minimal set of "
         "transcripts to contain all de novos.")
     
-    args = parser.parse_args()
-    
-    return args.input, args.output, args.deprecated_genes, \
-        args.cache_folder, args.genome_build.lower(), args.all_transcripts, \
-        args.minimise_transcripts
+    return parser.parse_args()
 
 def main():
     
-    input_file, output_file, old_gene_id_file, cache_dir, genome_build, \
-        all_transcripts, minimal_transcripts = get_options()
+    args = get_options()
+    ensembl = EnsemblRequest(args.cache_folder, args.genome_build)
+    de_novos = load_de_novos(args.de_novos, exclude_indels=False)
     
-    # load all the data
-    ensembl = EnsemblRequest(cache_dir, genome_build)
-    
-    old_gene_ids = {}
-    if old_gene_id_file is not None:
-        old_gene_ids = get_deprecated_gene_ids(old_gene_id_file)
-    
-    known_de_novos = load_de_novos(input_file, exclude_indels=False)
-    
-    output = open(output_file, "w")
+    output = open(args.out, "w")
     output.write("hgnc_symbol\ttranscript_id\tlength\tde_novos\n")
     
-    for gene_id in sorted(known_de_novos):
+    for gene_id in sorted(de_novos):
         print(gene_id)
-        de_novos = known_de_novos[gene_id]
+        de_novos = de_novos[gene_id]
         func_events = de_novos["missense"] + de_novos["nonsense"]
-        
-        # fix HGNC IDs that have been discontinued in favour of other gene IDs
-        if gene_id in old_gene_ids:
-            gene_id = old_gene_ids[gene_id]
         
         # find the counts per transcript, depending on whether we want to count
         # for all transcripts containing one or more de novos, or to find the
         # minimum set of transcripts to contain the de novos
         try:
-            if all_transcripts:
+            if args.all_transcripts:
                 counts = count_de_novos_per_transcript(ensembl, gene_id, func_events)
-            elif minimal_transcripts:
+            elif args.minimal_transcripts:
                 counts = minimise_transcripts(ensembl, gene_id, func_events)
         except (ValueError, IndexError):
             print("error occured with {0}".format(gene_id))
@@ -89,7 +67,8 @@ def main():
         
         # write the transcript details to a file
         for key in counts:
-            line = "{0}\t{1}\t{2}\t{3}\n".format(gene_id, key, counts[key]["len"], counts[key]["n"])
+            line = "{}\t{}\t{}\t{}\n".format(gene_id, key, counts[key]["len"],
+                counts[key]["n"])
             output.write(line)
         
     output.close()
