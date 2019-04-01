18aa08e520c010b6d0a2d87a03b2a483cd7dc73b
Jeremy McRae <jmcrae@illumina.com>
2019-04-01 11:57:29 -0700

    access position attribute correctly

--- denovonear/__main__.py
+++ denovonear/__main__.py
@@ -128,7 +128,7 @@ def get_mutation_rates(transcripts, mut_dict, ensembl):
     if combined is None:
         raise ValueError('no tx found')
     
-    length = combined.get_coding_distance(combined.get_cds_end())
+    length = combined.get_coding_distance(combined.get_cds_end())['pos']
     
     return rates, combined, length
 
