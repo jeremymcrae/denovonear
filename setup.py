576347fd13f76766ca5d52c98c5f3250bb724410
Jeremy McRae <jmcrae@illumina.com>
2020-03-26 11:39:24 -0700

    fix compilation on macos

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ from distutils.core import Extension
 from Cython.Build import cythonize
 
 EXTRA_COMPILE_ARGS = ["-std=c++11"]
+EXTRA_LINK_ARGS = []
 
 if sys.platform == "darwin":
-    EXTRA_COMPILE_ARGS += ["-stdlib=libc++"]
+    EXTRA_COMPILE_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
+    EXTRA_LINK_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
 
 weights = cythonize([
     Extension("denovonear.weights",
         extra_compile_args=EXTRA_COMPILE_ARGS,
+        extra_link_args=EXTRA_LINK_ARGS,
         sources=["denovonear/weights.pyx",
             "src/weighted_choice.cpp",
             "src/simulate.cpp"],
@@ -21,6 +24,7 @@ weights = cythonize([
         language="c++"),
     Extension("denovonear.transcript",
         extra_compile_args=EXTRA_COMPILE_ARGS,
+        extra_link_args=EXTRA_LINK_ARGS,
         sources=[
             "denovonear/transcript.pyx",
             "src/tx.cpp"],
@@ -28,6 +32,7 @@ weights = cythonize([
         language="c++"),
     Extension("denovonear.site_specific_rates",
         extra_compile_args=EXTRA_COMPILE_ARGS,
+        extra_link_args=EXTRA_LINK_ARGS,
         sources=[
             "denovonear/site_specific_rates.pyx",
             "src/weighted_choice.cpp",
@@ -41,7 +46,7 @@ setup(name="denovonear",
     description='Package to examine de novo clustering',
     long_description=io.open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    version="0.8.3",
+    version="0.8.4",
     author="Jeremy McRae",
     author_email="jeremy.mcrae@gmail.com",
     license="MIT",
