28aced1b2511ed086b9551978524994c5a00c7f0
Jeremy McRae <jmcrae@illumina.com>
2021-12-11 14:52:58 -0800

    better handle extending CDS for stop and start codons

--- src/gencode.cpp
+++ src/gencode.cpp
@@ -24,6 +24,51 @@ void sort_exons(std::vector<std::vector<int> > & exons) {
     std::sort(exons.begin(), exons.end(), compareExons);
 }
 
+// find the index of the exon containing a given chromosome position
+uint get_exon_num(std::vector<std::vector<int> > exons, int pos) {
+    for (uint i=0; i<exons.size(); i++) {
+        if (pos >= exons[i][0] && pos <= exons[i][1]) {
+            return i;
+        }
+    }
+    throw std::invalid_argument("can't find exon for an end codon");
+}
+
+// adjust CDS coords for any end codon positions
+// 
+// the CDS might exclude the start codon and end codon from the CDS range
+// we have collected the start and end codon ends in cds_range. We can't
+// just set the first CDS coord and last CDS coord to their values though,
+// as at least one stop codon spans an intron boundary, which messes up the
+// CDS if included as is.
+void include_end_codons(std::map<std::string, int> cds_range, TxInfo & info) {
+    sort_exons(info.cds);
+    sort_exons(info.exons);
+
+    // handle left (5') boundary
+    uint first_idx = get_exon_num(info.exons, info.cds[0][0]);
+    uint min_idx = get_exon_num(info.exons, cds_range["min"]);
+    if (min_idx == first_idx) {
+        info.cds[0][0] = cds_range["min"];
+    } else {
+        info.cds[0][0] = info.exons[first_idx][0];  // extend existing CDS
+        std::vector<int> extra_cds = {cds_range["min"], info.exons[min_idx][1]};
+        auto it = info.cds.begin();
+        info.cds.insert(it, extra_cds);
+    }
+
+    // handle right (3') boundary
+    uint last_idx = get_exon_num(info.exons, info.cds.back()[1]);
+    uint max_idx = get_exon_num(info.exons, cds_range["max"]);
+    if (max_idx == last_idx) {
+        info.cds.back()[1] = cds_range["max"];
+    } else {
+        info.cds[1][1] = info.exons[last_idx][1];  // extend existing CDS
+        std::vector<int> extra_cds = {info.exons[max_idx][0], cds_range["max"]};
+        info.cds.push_back(extra_cds);
+    }
+}
+
 // collect all features for a transcript into a single object
 //
 // When we load lines from gencode GTF files, each line represents a single exon
@@ -62,9 +107,7 @@ void load_transcripts(std::vector<NamedTx> & transcripts, GTF &gtf_file, bool co
 
         if (tx_id != current) {
             // adjust CDS for start and stop codon coords
-            sort_exons(info.cds);
-            info.cds[0][0] = cds_range["min"];
-            info.cds.back()[1] = cds_range["max"];
+            include_end_codons(cds_range, info);
             Tx tx = Tx(info.name, info.chrom, info.start, info.end, info.strand[0]);
             tx.set_exons(info.exons, info.cds);
             tx.set_cds(info.cds);
@@ -104,9 +147,7 @@ void load_transcripts(std::vector<NamedTx> & transcripts, GTF &gtf_file, bool co
 
     // also include the final transcript (if it transcript exists)
     if (info.name != "") {
-        sort_exons(info.cds);
-        info.cds[0][0] = cds_range["min"];
-        info.cds.back()[1] = cds_range["max"];
+        include_end_codons(cds_range, info);
         Tx tx = Tx(info.name, info.chrom, info.start, info.end, info.strand[0]);
         tx.set_exons(info.exons, info.cds);
         tx.set_cds(info.cds);
