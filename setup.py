8a441009ee98fd42d4f0ef3e2437261beea81a6f
Jeremy McRae <jmcrae@illumina.com>
2023-03-10 14:13:27 -0800

    added fixes for building on macos and windows

--- setup.py
+++ setup.py
@@ -2,16 +2,79 @@
 import sys
 import os
 import io
+import glob
+
+from distutils.ccompiler import new_compiler
 from setuptools import setup
 from distutils.core import Extension
 from Cython.Build import cythonize
 
-EXTRA_COMPILE_ARGS = ["-std=c++11"]
+EXTRA_COMPILE_ARGS = ['-std=c++11', '-I/usr/include']
 EXTRA_LINK_ARGS = []
-
 if sys.platform == "darwin":
-    EXTRA_COMPILE_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
-    EXTRA_LINK_ARGS += ["-stdlib=libc++", "-mmacosx-version-min=10.9"]
+    EXTRA_COMPILE_ARGS += [
+        "-stdlib=libc++",
+        "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1",
+        "-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include",
+        ]
+    EXTRA_LINK_ARGS += [
+        "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib",
+        ]
+
+def build_zlib():
+    ''' compile zlib code to object files for linking on windows
+    
+    Returns:
+        list of paths to compiled object code
+    '''
+    include_dirs = ['src/zlib/']
+    sources = list(glob.glob('src/zlib/*.c'))
+    extra_compile_args = []
+    
+    cc = new_compiler()
+    return cc.compile(sources, include_dirs=include_dirs,
+        extra_preargs=extra_compile_args)
+
+def get_gzstream_path():
+    ''' workaround for building gzstream on windows
+    cython on windows didn't like the .C extension for gzstream. This just
+    renames the file (on windows only), and returns the relative path.
+    '''
+    gzstream_path = 'src/gzstream/gzstream.C'
+    if sys.platform == 'win32':
+        gzstream_win_path = 'src/gzstream/gzstream.cpp'
+        try:
+            os.rename(gzstream_path, gzstream_win_path)
+        except FileNotFoundError:
+            pass  # avoid error on github actions
+        gzstream_path = gzstream_win_path
+    return gzstream_path
+
+def scrub_gzstream():
+    ''' workaround for compilation error on macos
+    
+    compiling gzstream requires the corresponding gzstream.h file, but if we 
+    include the gzstream directory in the include dirs, then clang complains
+    about the version file in the gzstream folder. If we remove the gzstream
+    directory from the include dirs, then clang complains about the missing
+    gzstream.h. This is because gzstream.C identifies it's header file with
+    angle brackets. Replacing the angle brackets in that line seems to work.
+    '''
+    with open(get_gzstream_path(), 'rt') as handle:
+        lines = handle.readlines()
+    
+    with open(get_gzstream_path(), 'wt') as handle:
+        for line in lines:
+            if line == '#include <gzstream.h>\n':
+                line = '#include "gzstream.h"\n'
+            handle.write(line)
+
+if sys.platform == 'win32':
+    zlib, libs = build_zlib(), []
+else:
+    zlib, libs = [], ['z']
+
+scrub_gzstream()
 
 weights = cythonize([
     Extension("denovonear.weights",
@@ -40,7 +103,7 @@ weights = cythonize([
             "src/tx.cpp",
             "src/gzstream/gzstream.C",
         ],
-        include_dirs=["src/", "src/gzstream"],
+        include_dirs=["src/"],
         libraries=["z"],
         language="c++"),
     Extension("denovonear.site_specific_rates",
