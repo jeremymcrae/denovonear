826f7fedbf93dfc7ea79b15a3ea286c7c3280891
Jeremy McRae <jmcrae@illumina.com>
2023-03-11 19:44:01 -0800

    removed extra logging

--- src/denovonear/gencode.pyx
+++ src/denovonear/gencode.pyx
@@ -195,11 +195,6 @@ cdef class Gene:
             seq = None
         if seq is None and __genome_ is not None:
             seq = __genome_[chrom][start-1-offset:end-1+offset].seq.upper()
-            print(f'getting fasta seq on {chrom}:{start}-{end}, (offset: {offset}), which is {start-1-offset} to {end-1+offset}: {__genome_[chrom][start-1-offset:end-1+offset]}')
-            print(f'constructing tx (offset={offset}), genomic seq from fasta: {seq}')
-            ####################################################
-            # NOTE: this might be where it goes wrong
-            ####################################################
         strand = chr(tx.get_strand())
         if strand == '-' and seq is not None:
             seq = tx.reverse_complement(seq.encode('utf8')).decode('utf8')
